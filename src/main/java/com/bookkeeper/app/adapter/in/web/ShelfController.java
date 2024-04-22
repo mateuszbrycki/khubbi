838f82f4da74f8ad05155506cdb9ed7accd02869
mateuszbrycki <mateusz.brycki3@gmail.com>
2024-04-22 21:20:17 +0200

    master - Tested ShelfController

--- src/main/java/com/bookkeeper/app/adapter/in/web/ShelfController.java
+++ src/main/java/com/bookkeeper/app/adapter/in/web/ShelfController.java
@@ -2,6 +2,7 @@ package com.bookkeeper.app.adapter.in.web;
 
 import com.bookkeeper.app.application.port.in.AddShelfUseCase;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +19,7 @@ public class ShelfController {
     this.addShelfUseCase = addShelfUseCase;
   }
 
-  @PostMapping
+  @PostMapping(consumes = "application/json", produces = "application/json")
   public ResponseEntity<?> addShelf(@RequestBody Shelf shelf) {
 
     AddShelfUseCase.AddShelfCommand addShelfCommand =
---
+++ src/test/java/com/bookkeeper/app/adapter/in/web/ShelfControllerTest.java
@@ -0,0 +1,89 @@
+package com.bookkeeper.app.adapter.in.web;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.bookkeeper.app.adapter.in.web.security.JwtAuthenticationFilter;
+import com.bookkeeper.app.application.domain.model.Shelf;
+import com.bookkeeper.app.application.port.in.AddShelfUseCase;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.vavr.control.Try;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+@WebMvcTest(
+    value = ShelfController.class,
+    excludeAutoConfiguration = {
+      SecurityAutoConfiguration.class,
+      SecurityFilterAutoConfiguration.class
+    },
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = JwtAuthenticationFilter.class)
+)
+class ShelfControllerTest {
+  @Autowired private MockMvc mockMvc;
+
+  @MockBean private AddShelfUseCase addShelfUseCase;
+
+  @Test
+  public void shouldReturnRequestErrorWhenShelfAdditionFailed() throws Exception {
+
+    // given
+    when(addShelfUseCase.addShelf(new AddShelfUseCase.AddShelfCommand("any-name")))
+        .thenReturn(Try.failure(new Exception("Cannot add a shelf")));
+
+    // when
+    this.mockMvc
+        .perform(
+            post("/shelf")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(new ShelfController.Shelf("any-name"))))
+            .andDo(print())
+            .andExpect(status().isConflict())
+        .andExpect(content().string(containsString("Cannot add a shelf")));
+  }
+
+  @Test
+  public void shouldReturnCreatedWhenShelfAdditionSucceeded() throws Exception {
+
+    // given
+    UUID id = UUID.randomUUID();
+    AddShelfUseCase.Shelf shelf = new AddShelfUseCase.Shelf(id, "shelf-name");
+    when(addShelfUseCase.addShelf(new AddShelfUseCase.AddShelfCommand(shelf.name())))
+        .thenReturn(Try.success(shelf));
+
+    // when
+    this.mockMvc
+        .perform(
+            post("/shelf")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(new ShelfController.Shelf(shelf.name()))))
+            .andDo(print())
+            .andExpect(status().isCreated())
+        .andExpect(content().string(containsString(id.toString())))
+        .andExpect(content().string(containsString(shelf.name())))
+    ;
+  }
+
+  private final String asJsonString(Object obj) {
+    try {
+      return new ObjectMapper().writeValueAsString(obj);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
