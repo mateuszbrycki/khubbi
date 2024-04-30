9b3275693d4466f2732c580cdec12492e81a6946
mateuszbrycki <mateusz.brycki3@gmail.com>
2024-04-30 22:20:14 +0200

    master - Store Jest Test Results

---
+++ src/main/front/jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  modulePaths: ['<rootDir>'],
+  testEnvironment: 'jsdom',
+  reporters: [ "default", "jest-junit" ]
+};
