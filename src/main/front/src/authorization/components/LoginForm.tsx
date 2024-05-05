b1938b27437ff6070e7adfec46b476c838c2a15b
mateuszbrycki <mateusz.brycki3@gmail.com>
2024-05-05 20:08:38 +0200

    master - Clear forms onSubmit

--- src/main/front/src/authorization/components/LoginForm.tsx
+++ src/main/front/src/authorization/components/LoginForm.tsx
@@ -18,21 +18,27 @@ const RegisterForm: React.FC<LoginFormProps & LoginFormActionProps> = (props) =>
     const [email, setEmail] = useState<string>("")
     const [password, setPassword] = useState<string>("")
 
+    const resetFormValues = () => {
+        setEmail("");
+        setPassword("");
+    }
+
     return <>
         <h2>Login</h2>
         <Form onSubmit={event => {
             loginUser(email, password);
             event.preventDefault();
+            resetFormValues();
         }}>
             <Form.Group className="mb-3" controlId="loginFormEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="text" placeholder="Enter email"
+                <Form.Control type="text" value={email} placeholder="Enter email"
                               onChange={(event) => setEmail(event.target.value)}/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="loginFormPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password"
+                <Form.Control type="password" value={password} placeholder="Password"
                               onChange={(event) => setPassword(event.target.value)}/>
             </Form.Group>
             <Button variant="primary" type="submit">
--- src/main/front/src/authorization/components/RegisterForm.tsx
+++ src/main/front/src/authorization/components/RegisterForm.tsx
@@ -28,15 +28,23 @@ const RegisterForm: React.FC<RegisterFormProps & RegisterFormActionProps> = (pro
         setValidationError("");
     }, [password, repeatPassword]);
 
+    const resetFormValues = () => {
+        setEmail("");
+        setPassword("");
+        setRepeatPassword("");
+        setValidationError("");
+    }
+
     return <>
         <h2>Register</h2>
         <Form onSubmit={event => {
             registerUser(email, password);
             event.preventDefault();
+            resetFormValues();
         }}>
             <Form.Group className="mb-3" controlId="registerFormEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="text" placeholder="Enter email"
+                <Form.Control type="text" value={email} placeholder="Enter email"
                               onChange={(event) => setEmail(event.target.value)}
                               required
                 />
@@ -44,13 +52,13 @@ const RegisterForm: React.FC<RegisterFormProps & RegisterFormActionProps> = (pro
 
             <Form.Group className="mb-3" controlId="registerFormPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password"
+                <Form.Control type="password" value={password} placeholder="Password"
                               onChange={(event) => setPassword(event.target.value)} required/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="registerFormPasswordRepeat">
                 <Form.Label>Repeat Password</Form.Label>
-                <Form.Control type="password" placeholder="Repeat Password"
+                <Form.Control type="password" value={repeatPassword} placeholder="Repeat Password"
                               onChange={(event) => setRepeatPassword(event.target.value)} required/>
                 <Form.Control.Feedback type="invalid">
                     Passwords are not equal.
