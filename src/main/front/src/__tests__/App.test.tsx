2af0b5017b3e7a4c0ee85da46843ab48b4828f9e
mateuszbrycki <mateusz.brycki3@gmail.com>
2024-05-23 19:43:02 +0200

    master - Add App Children Nodes Tests

--- src/main/front/src/__tests__/App.test.tsx
+++ src/main/front/src/__tests__/App.test.tsx
@@ -6,3 +6,9 @@ it('renders correctly', () => {
     renderer.render(<App/>)
     expect(renderer.getRenderOutput()).toMatchSnapshot()
 });
+
+it('renders correctly with child component', () => {
+    const renderer = ShallowRenderer.createRenderer()
+    renderer.render(<App><h1>Child Component</h1></App>)
+    expect(renderer.getRenderOutput()).toMatchSnapshot()
+});
