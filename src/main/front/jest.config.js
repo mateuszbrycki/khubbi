ab002e53170ca151348b46e7517a78645828247d
mateuszbrycki <mateusz.brycki3@gmail.com>
2024-07-19 23:40:00 +0200

    master - Revert Changes

---
+++ src/main/front/jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  modulePaths: ['<rootDir>'],
+  testEnvironment: 'jsdom',
+  reporters: [ "default", "jest-junit" ],
+  moduleNameMapper: {
+    "^axios$": "axios/dist/node/axios.cjs"
+  }
+};
